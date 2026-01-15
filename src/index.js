c7354a34478f52352497bd3c54e28c9da024aa99
Mikaemik <mikael.e.mikkola@gmail.com>
2026-01-15 11:46:33 +0200

    lisätty viikko1 tehtävät

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
-import http from 'http';
+import express from 'express';
 const hostname = '127.0.0.1';
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log('Request:', req.method, req.url);
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(`<h1>Welcome to my REST API!</h1><p>Haettu URL: ${req.url}</p>`);
+// Dummy mock data (nollautuu aina, kun sovelluksen käynnistää uudelleen)
+const items = [
+  {id: 1, name: 'Omena'},
+  {id: 2, name: 'Banaani'},
+  {id: 3, name: 'Persikka'},
+];
+
+// parsitaan json data pyynnostä ja lisätään request-objektiin
+app.use(express.json());
+
+// API ROOT
+app.get('/', (req, res) => {
+  res.send('This is dummy items API!');
 });
 
-server.listen(port, hostname, () => {
+// Get all items
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+// Get item based on id
+app.get('/items/:id', (req, res) => {
+  console.log('getting item id:', req.params.id);
+  const itemFound = items.find((item) => item.id == req.params.id);
+  if (itemFound) {
+    res.json(itemFound);
+  } else {
+    res.status(404).json({message: 'item not found'});
+  }
+  //res.json(itemFound);
+});
+
+// TODO: add PUT route for items
+
+// TODO: add DELETE route for items
+
+// Add new item
+app.post('/items', (req, res) => {
+  //console.log('add item request body', req.body);
+  // TODO: lisää id listaan lisättävälle objektille
+  items.push(req.body);
+  res.status(201).json({message: 'new item added'});
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
