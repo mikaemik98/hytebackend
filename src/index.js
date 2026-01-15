b357acf159f0425db328900e6abd9114ab14f034
Mikaemik <mikael.e.mikkola@gmail.com>
2026-01-15 10:51:12 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Terve taas');
+import http from 'http';
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log('Request:', req.method, req.url);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(`<h1>Welcome to my REST API!</h1><p>Haettu URL: ${req.url}</p>`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
