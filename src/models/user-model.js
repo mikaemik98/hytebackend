f0361eb6819b9132cb857aa1d6661cebc2ebc523
Mikaemik <mikael.e.mikkola@gmail.com>
2026-02-05 11:32:14 +0200

    korjattu user_model funktio

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -9,7 +9,7 @@ POST /api/users - add a new user
 //Huom: virheenkäsittely puuttuu
 const findUserByUsername = async (username) => {
   const sql = 'SELECT * FROM Users WHERE username = ?';
-  const [rows] = promisePool.execute(sql, [username]);
+  const [rows] = await promisePool.execute(sql, [username]);
   return rows[0];
 };
 
